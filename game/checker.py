f46eb80183557bc4ee8e159bb263ddbf129e2336
Ricky Grullon <rgrullon8280@gmail.com>
2021-11-04 23:31:44 -0400

    Fix moving checker feature

--- game/checker.py
+++ game/checker.py
@@ -12,14 +12,9 @@ class Checker:
         self.num_of_point: int = num_of_point
     
     def move_to(self,point):
-        FPS = 60
         self.num_of_point = point.number
-        x_step:float = (point.x3 - self.x )/FPS
-        new_y:float = self.calc_y(len(point.checkers),point.y1)
-        y_step:float = (new_y - self.y)/FPS
-        while (self.x != point.x3) and (self.y != new_y):
-            self.x += x_step
-            self.y += y_step
+        self.x = point.x3
+        self.y = self.calc_y(len(point.checkers),point.y1)
         
 
 
